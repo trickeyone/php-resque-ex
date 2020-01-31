bee4f1ceb2c71e992472ecb792f9026919a975c4
Josh England <trickeyone@users.noreply.github.com>
2020-01-31 13:57:26 -0800

    Fixed not checking fwrite return properly
    Fixed not checking if fwrite returns zero or false for a failed write operation

--- lib/Redisent/Redisent.php
+++ lib/Redisent/Redisent.php
@@ -75,7 +75,7 @@ class Redisent {
         /* Open a Redis connection and execute the command */
         for ($written = 0; $written < strlen($command); $written += $fwrite) {
             $fwrite = fwrite($this->__sock, substr($command, $written));
-            if ($fwrite === FALSE) {
+            if ($fwrite === FALSE || $fwrite === 0) {
                 throw new Exception('Failed to write entire command to stream');
             }
         }
